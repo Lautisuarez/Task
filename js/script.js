7c42466f24696a6baecdf8ab276f248388e9d659
lautaro <lauti_campeon7@hotmail.com>
2021-02-20 22:09:10 -0300

    Lógica de agregar y eliminar tareas

---
+++ js/script.js
@@ -0,0 +1,58 @@
+document.getElementById('form-task').addEventListener('submit', saveTask);
+
+function saveTask(e){
+    e.preventDefault();
+    let title = document.getElementById('title').value;
+    let description = document.getElementById('description').value;
+
+    const task = {
+        title, // title: title
+        description // description: description
+    };
+
+    if(localStorage.getItem('tasks') === null){
+        let tasks = [];
+        tasks.push(task);
+        localStorage.setItem('tasks', JSON.stringify(tasks));
+    } else{
+        let tasks = JSON.parse(localStorage.getItem('tasks'));
+        tasks.push(task);
+        localStorage.setItem('tasks', JSON.stringify(tasks));
+    }
+
+    getTasks();
+    document.getElementById('form-task').reset();
+}
+
+function getTasks(){
+    let tasks = JSON.parse(localStorage.getItem('tasks'));
+    let tasksView = document.getElementById('tasks');
+
+    tasksView.innerHTML = '';
+
+    for(let i=0; i<tasks.length; i++){
+        let title = tasks[i].title;
+        let description = tasks[i].description;
+
+        tasksView.innerHTML += `<div class="item">
+            <div class="contenido-item">
+                <p>${title} - ${description}</p>
+                <a class="btn btn-eliminar" onclick="deleteTasks('${title}')">Eliminar</a>
+            </div>
+        </div>`
+    }
+}
+
+function deleteTasks(title){
+    let tasks = JSON.parse(localStorage.getItem('tasks'));
+
+    for(let i=0; i<tasks.length; i++){
+        if(tasks[i].title == title){
+            tasks.splice(i, 1);
+        }
+    }
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+    getTasks();
+}
+
+getTasks();
